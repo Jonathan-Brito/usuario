4f2641a2f303b8a83b001c2b5e56ade4d06183f5
Jonathan Brito Costa <jonathan.costa@itsgroup.com.br>
2025-06-24 23:29:35 -0300

    Adicionado classes de metodo de salvar e deletar e de buscar e corrigido metodo de security e exception e classe controller

--- src/main/java/br/com/brito/usuario/business/UsuarioService.java
+++ src/main/java/br/com/brito/usuario/business/UsuarioService.java
@@ -4,8 +4,17 @@ import br.com.brito.usuario.business.converter.UsuarioConverter;
 import br.com.brito.usuario.business.dto.UsuarioDTO;
 import br.com.brito.usuario.infrastructure.entity.Usuario;
 import br.com.brito.usuario.infrastructure.exceptions.ConflictException;
+import br.com.brito.usuario.infrastructure.exceptions.ResourceNotFoundException;
+import br.com.brito.usuario.infrastructure.exceptions.UnauthorizedException;
 import br.com.brito.usuario.infrastructure.repository.UsuarioRepository;
+import br.com.brito.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +25,8 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
 
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO) {
@@ -25,6 +36,19 @@ public class UsuarioService {
         return usuarioConverter.paraUsuarioDTO(
                 usuarioRepository.save(usuario)
         );
+    }
+
+    public String autenticarUsuario(UsuarioDTO usuarioDTO) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(),
+                            usuarioDTO.getSenha())
+            );
+            return "Bearer " + jwtUtil.generateToken(authentication.getName());
+
+        } catch (BadCredentialsException | UsernameNotFoundException | AuthorizationDeniedException e) {
+            throw new UnauthorizedException("Usuário ou senha inválidos: ", e.getCause());
+        }
     }
 
     public void emailExiste(String email) {
@@ -41,6 +65,25 @@ public class UsuarioService {
     public boolean verificaEmailExistente(String email) {
 
         return usuarioRepository.existsByEmail(email);
+    }
+
+    public UsuarioDTO buscarUsuarioPorEmail(String email) {
+        try {
+            return usuarioConverter.paraUsuarioDTO(
+                    usuarioRepository.findByEmail(email)
+                            .orElseThrow(
+                                    () -> new ResourceNotFoundException("Email não encontrado " + email)
+                            )
+            );
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("Email não encontrado " + email);
+        }
+    }
+
+
+    public void deletaUsuarioPorEmail(String email) {
+
+        usuarioRepository.deleteByEmail(email);
     }
 
 }
--- src/main/java/br/com/brito/usuario/controller/UsuarioController.java
+++ src/main/java/br/com/brito/usuario/controller/UsuarioController.java
@@ -4,10 +4,7 @@ import br.com.brito.usuario.business.UsuarioService;
 import br.com.brito.usuario.business.dto.UsuarioDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +16,22 @@ public class UsuarioController {
     @PostMapping
     public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO) {
         return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UsuarioDTO usuarioDTO) {
+        return ResponseEntity.ok(usuarioService.autenticarUsuario(usuarioDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<UsuarioDTO> buscaUsuarioPorEmail(@RequestParam("email") String email) {
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deletaUsuarioPorEmail(@PathVariable String email) {
+        usuarioService.deletaUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
     }
 
 }
